e4a1ae14d7310fbe2225525d3085a370001fc08e
GearIntellix <luthfigearintellix@gmail.com>
2020-10-24 05:40:05 +0700

    new fn raw-response-message and raw-response-message-context
    Signed-off-by: GearIntellix <luthfigearintellix@gmail.com>

--- chat.ex.go
+++ chat.ex.go
@@ -7,9 +7,8 @@ import (
 	"net/http"
 )
 
-// RawPostMessageArguments chat.postMessage payload
-type RawPostMessageArguments struct {
-	Endpoint       string        `json:"-"`
+// RawSendMessageArguments chat.postMessage payload
+type RawSendMessageArguments struct {
 	Token          string        `json:"token,omitempty"`
 	Channel        string        `json:"channel,omitempty"`
 	Text           string        `json:"text,omitempty"`
@@ -28,43 +27,22 @@ type RawPostMessageArguments struct {
 	UserName       string        `json:"username,omitempty"`
 }
 
-// RawPostMessage do chat.postMessage with highly customizable
-func (api *Client) RawPostMessage(args RawPostMessageArguments) (_channel string, _timestamp string, _text string, err error) {
-	return api.RawPostMessageContext(context.Background(), args)
+// RawSendMessage do chat.postMessage with highly customizable
+func (api *Client) RawSendMessage(args RawSendMessageArguments) (_channel string, _timestamp string, _text string, err error) {
+	return api.RawSendMessageContext(context.Background(), args)
 }
 
-// RawPostMessageContext do chat.postMessage with highly customizable with context
-func (api *Client) RawPostMessageContext(ctx context.Context, args RawPostMessageArguments) (_channel string, _timestamp string, _text string, err error) {
+// RawSendMessageContext do chat.postMessage with highly customizable and context
+func (api *Client) RawSendMessageContext(ctx context.Context, args RawSendMessageArguments) (_channel string, _timestamp string, _text string, err error) {
 	var (
 		req      *http.Request
-		parser   func(*chatResponseFull) responseParser
 		response chatResponseFull
-
-		endpoint = api.endpoint + string(chatPostMessage)
 	)
 
-	switch {
-	case args.Endpoint != "":
-		args.Token = ""
-		req, err = jsonReq(args.Endpoint, args)
-		if err != nil {
-			return _channel, _timestamp, _text, err
-		}
-
-		parser = func(resp *chatResponseFull) responseParser {
-			return newContentTypeParser(resp)
-		}
-
-	default:
-		args.Token = api.token
-		req, err = jsonReq(endpoint, args)
-		if err != nil {
-			return _channel, _timestamp, _text, err
-		}
-
-		parser = func(resp *chatResponseFull) responseParser {
-			return newJSONParser(resp)
-		}
+	args.Token = api.token
+	req, err = jsonReq(api.endpoint+string(chatPostMessage), args)
+	if err != nil {
+		return _channel, _timestamp, _text, err
 	}
 
 	if api.Debug() {
@@ -76,7 +54,57 @@ func (api *Client) RawPostMessageContext(ctx context.Context, args RawPostMessag
 		api.Debugf("Sending request: %s", string(reqBody))
 	}
 
-	if err = doPost(ctx, api.httpclient, req, parser(&response), api); err != nil {
+	if err = doPost(ctx, api.httpclient, req, newJSONParser(&response), api); err != nil {
+		return _channel, _timestamp, _text, err
+	}
+
+	_channel = response.Channel
+	_timestamp = response.getMessageTimestamp()
+	_text = response.Text
+	err = response.Err()
+
+	return _channel, _timestamp, _text, err
+}
+
+// RawResponseMessageArguments hook message payload
+type RawResponseMessageArguments struct {
+	Endpoint        string        `json:"-"`
+	ThreadTS        string        `json:"thread_ts,omitempty"`
+	Text            string        `json:"text,omitempty"`
+	Attachments     []interface{} `json:"attachments,omitempty"`
+	Blocks          []interface{} `json:"blocks,omitempty"`
+	ResponseType    string        `json:"response_type,omitempty"`
+	ReplaceOriginal bool          `json:"replace_original"`
+	DeleteOriginal  bool          `json:"delete_original"`
+}
+
+// RawResponseMessage do hook message with highly customizable
+func (api *Client) RawResponseMessage(args RawResponseMessageArguments) (_channel string, _timestamp string, _text string, err error) {
+	return api.RawResponseMessageContext(context.Background(), args)
+}
+
+// RawResponseMessageContext do hook message with highly customizable and context
+func (api *Client) RawResponseMessageContext(ctx context.Context, args RawResponseMessageArguments) (_channel string, _timestamp string, _text string, err error) {
+	var (
+		req      *http.Request
+		response chatResponseFull
+	)
+
+	req, err = jsonReq(args.Endpoint, args)
+	if err != nil {
+		return _channel, _timestamp, _text, err
+	}
+
+	if api.Debug() {
+		reqBody, err := ioutil.ReadAll(req.Body)
+		if err != nil {
+			return _channel, _timestamp, _text, err
+		}
+		req.Body = ioutil.NopCloser(bytes.NewBuffer(reqBody))
+		api.Debugf("Sending request: %s", string(reqBody))
+	}
+
+	if err = doPost(ctx, api.httpclient, req, newContentTypeParser(&response), api); err != nil {
 		return _channel, _timestamp, _text, err
 	}
 
