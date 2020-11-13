afc821af5406968b4931baec28bdd006674a3be2
GearIntellix <luthfigearintellix@gmail.com>
2020-11-13 21:36:37 +0700

    adding update.view
    Signed-off-by: GearIntellix <luthfigearintellix@gmail.com>

--- ex.views.go
+++ ex.views.go
@@ -5,9 +5,8 @@ import (
 	"encoding/json"
 )
 
-// OpenRawViewArguments views.open payload
-type OpenRawViewArguments struct {
-	TriggerID       string        `json:"-"`
+// RawViewObject view payload
+type RawViewObject struct {
 	Type            ViewType      `json:"type"`
 	Title           interface{}   `json:"title"`
 	Blocks          []interface{} `json:"blocks"`
@@ -20,6 +19,12 @@ type OpenRawViewArguments struct {
 	ExternalID      string        `json:"external_id,omitempty"`
 }
 
+// OpenRawViewArguments views.open payload
+type OpenRawViewArguments struct {
+	TriggerID string        `json:"trigger_id"`
+	View      RawViewObject `json:"view"`
+}
+
 // OpenRawViewContext do views.open with highly customizable
 func (api *Client) OpenRawView(args OpenRawViewArguments) (resp *ViewResponse, err error) {
 	return api.OpenRawViewContext(context.Background(), args)
@@ -28,18 +33,57 @@ func (api *Client) OpenRawView(args OpenRawViewArguments) (resp *ViewResponse, e
 // OpenRawViewContext do views.open with highly customizable and context
 func (api *Client) OpenRawViewContext(ctx context.Context, args OpenRawViewArguments) (resp *ViewResponse, err error) {
 	var encoded []byte
-	encoded, err = json.Marshal(map[string]interface{}{
-		"trigger_id": args.TriggerID,
-		"view":       args,
-	})
+	encoded, err = json.Marshal(args)
 	if err != nil {
 		return resp, err
+	}
+
+	if api.Debug() {
+		api.Debugf("Sending request views.open: %s", string(encoded))
 	}
 
 	resp = &ViewResponse{}
 
 	endpoint := api.endpoint + "views.open"
 	err = postJSON(ctx, api.httpclient, endpoint, api.token, encoded, &resp, api)
+	if err != nil {
+		return resp, err
+	}
+
+	return resp, resp.Err()
+}
+
+// UpdateRawViewArguments views.update payload
+type UpdateRawViewArguments struct {
+	ExternalID string        `json:"external_id,omitempty"`
+	Hash       string        `json:"hash,omitempty"`
+	ViewID     string        `json:"view_id,omitempty"`
+	View       RawViewObject `json:"view"`
+}
+
+// UpdateRawView do views.update with highly customizable
+func (api *Client) UpdateRawView(args UpdateRawViewArguments) (resp *ViewResponse, err error) {
+	return api.UpdateRawViewContext(context.Background(), args)
+}
+
+// UpdateRawViewContext do views.update with highly customizable and context
+func (api *Client) UpdateRawViewContext(ctx context.Context, args UpdateRawViewArguments) (resp *ViewResponse, err error) {
+	if args.ExternalID == "" && args.ViewID == "" {
+		return resp, ErrParametersMissing
+	}
+
+	var encoded []byte
+	encoded, err = json.Marshal(args)
+	if err != nil {
+		return resp, err
+	}
+
+	if api.Debug() {
+		api.Debugf("Sending request views.update: %s", string(encoded))
+	}
+
+	endpoint := api.endpoint + "views.update"
+	err = postJSON(ctx, api.httpclient, endpoint, api.token, encoded, resp, api)
 	if err != nil {
 		return resp, err
 	}
